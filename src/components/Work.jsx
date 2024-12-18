636faa0ebcc5d1944168c8f012e2f176c609ce13
Damilola <damiekanola@gmail.com>
2024-12-18 22:59:38 +0100

    bugs again

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,11 +3,11 @@ import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 const Work = () => {
   return (
-    <div className="flex h-[400px] bg-[#4b5563] overflow-hidden rounded-md w-full m-auto ">
-      <div className=" p-10  h-full w-1/2">
+    <div className="flex max-[800px]:flex-col max-[800px]:justify-center h-full overflow-hidden rounded-md ">
+      <div className=" p-10 w-1/2 max-[800px]:w-3/4 bg-[#4b5563] flex-1">
         <img src="/images/incanto desktop.webp" />
       </div>
-      <div className="  bg-[#374151] p-8 w-1/2 h-full">
+      <div className="  bg-[#374151] p-8 w-1/2 max-[800px]:w-3/4 flex-1">
         <h2 className=" font-semibold text-xl mb-3">Incanto</h2>
         <p>
           A fully responsive e-commerce website featuring a shopping cart with
@@ -15,7 +15,7 @@ const Work = () => {
           for a real-world feel. Users can browse products, add them to their
           cart, and go through a smooth checkout process.
         </p>
-        <a href="https://react-incanto-clw8.vercel.app/">
+        <a href="https://react-incanto-clw8.vercel.app/" target="blank">
           <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
         </a>
       </div>
